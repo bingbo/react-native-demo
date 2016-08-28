e1cae2ff97a7999c97a812be5712ccdcec600739
bingbo <ibingbo.zh@gmail.com>
2016-08-28 09:59:06 +0800

    up

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # react-native-demo
 react-native-demo
+
+
+## 运行app
+
+* 在app/src/main下建assets文件夹
+* npm start
+* curl "http://localhost:8081/index.android.bundle?platform=android" -o "app/src/main/assets/index.android.bundle"
+* ./gradlew installDebug
+
+### Tip
+
+> minSdkVersion最小必须是16
+
---
+++ app/src/test/java/com/ibingbo/react_native_demo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.ibingbo.react_native_demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
